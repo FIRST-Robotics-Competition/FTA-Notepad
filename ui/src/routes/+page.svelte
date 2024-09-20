866c13d549df2b93840f8d6b56c88fd3171f29dd
Fred Silberberg <fred@silberberg.xyz>
2024-09-19 21:49:09 -0700

    Connect to the field monitor API (#14)
    * Connect to the field monitor API
    
    This adds connection to the signalr API, reflecting the current status of field. Note that to work, this will need the CORS changes I have in PR to FMS and is not available in a public offseason build yet.
    
    * Handle connection errors.
    
    * Remove dynamic href that cannot work with prerender.
    
    * Prettier
    
    * Revert "Remove dynamic href that cannot work with prerender."
    
    This reverts commit 205bbc88c456fda9938d11319f373b1429b79f0b.
    
    * Turn off prerender always, set a fallback page.

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { AllianceType, StationType, MonitorStatusType, DSStationStatus } from '../fms/fms-api';
-	import type { FieldMonitorData } from '../fms/fms-signalr';
+	import { AllianceType, StationType, MatchStateType } from '../fms/fms-api';
+	import { connectFieldMonitor, type FieldMonitorData } from '../fms/fms-signalr';
 	import MonitorRow from '../lib/components/field-monitor/MonitorRow.svelte';
 	import {
 		Blue1Default,
@@ -11,156 +11,107 @@
 		Red3Default
 	} from './defaults';
 
-	let teams: FieldMonitorData[] = [
-		{
-			teamNumber: 5678,
-			alliance: AllianceType.Blue,
-			station: StationType.Station1,
-			connection: false,
-			dsLinkActive: false,
-			radioLink: false,
-			rioLink: false,
-			linkActive: false,
-			isEnabled: false,
-			isAuto: false,
-			isBypassed: false,
-			isEStopPressed: false,
-			isEStopped: false,
-			battery: 0,
-			monitorStatus: MonitorStatusType.Unknown,
-			averageTripTime: 0,
-			lostPackets: 0,
-			signal: 0,
-			dataRateTotal: 0,
-			stationStatus: DSStationStatus.Unknown
-		},
-		{
-			teamNumber: 9876,
-			alliance: AllianceType.Blue,
-			station: StationType.Station2,
-			connection: true,
-			dsLinkActive: false,
-			radioLink: false,
-			rioLink: false,
-			linkActive: false,
-			isEnabled: false,
-			isAuto: false,
-			isBypassed: false,
-			isEStopPressed: false,
-			isEStopped: false,
-			battery: 0,
-			monitorStatus: MonitorStatusType.Unknown,
-			averageTripTime: 0,
-			lostPackets: 0,
-			signal: 0,
-			dataRateTotal: 0,
-			stationStatus: DSStationStatus.Unknown
-		},
-		{
-			teamNumber: 4321,
-			alliance: AllianceType.Blue,
-			station: StationType.Station3,
-			connection: true,
-			dsLinkActive: false,
-			radioLink: false,
-			rioLink: false,
-			linkActive: false,
-			isEnabled: false,
-			isAuto: false,
-			isBypassed: false,
-			isEStopPressed: false,
-			isEStopped: false,
-			battery: 0,
-			monitorStatus: MonitorStatusType.Unknown,
-			averageTripTime: 0,
-			lostPackets: 0,
-			signal: 0,
-			dataRateTotal: 0,
-			stationStatus: DSStationStatus.Waiting
-		},
-		{
-			teamNumber: 2468,
-			alliance: AllianceType.Red,
-			station: StationType.Station1,
-			connection: true,
-			dsLinkActive: true,
-			radioLink: false,
-			rioLink: false,
-			linkActive: false,
-			isEnabled: false,
-			isAuto: false,
-			isBypassed: false,
-			isEStopPressed: false,
-			isEStopped: false,
-			battery: 0,
-			monitorStatus: MonitorStatusType.Unknown,
-			averageTripTime: 0,
-			lostPackets: 0,
-			signal: 0,
-			dataRateTotal: 0,
-			stationStatus: DSStationStatus.Unknown
-		},
-		{
-			teamNumber: 1357,
-			alliance: AllianceType.Red,
-			station: StationType.Station2,
-			connection: true,
-			dsLinkActive: true,
-			radioLink: true,
-			rioLink: true,
-			linkActive: false,
-			isEnabled: false,
-			isAuto: false,
-			isBypassed: false,
-			isEStopPressed: false,
-			isEStopped: false,
-			battery: 0,
-			monitorStatus: MonitorStatusType.Unknown,
-			averageTripTime: 0,
-			lostPackets: 0,
-			signal: 0,
-			dataRateTotal: 0,
-			stationStatus: DSStationStatus.Unknown
-		},
-		{
-			teamNumber: 3242,
-			alliance: AllianceType.Red,
-			station: StationType.Station3,
-			connection: true,
-			dsLinkActive: true,
-			radioLink: true,
-			rioLink: true,
-			linkActive: true,
-			isEnabled: false,
-			isAuto: false,
-			isBypassed: false,
-			isEStopPressed: false,
-			isEStopped: false,
-			battery: 0,
-			monitorStatus: MonitorStatusType.Unknown,
-			averageTripTime: 0,
-			lostPackets: 0,
-			signal: 0,
-			dataRateTotal: 0,
-			stationStatus: DSStationStatus.Unknown
-		}
-	];
+	let blue1: FieldMonitorData = Blue1Default;
+	let blue2: FieldMonitorData = Blue2Default;
+	let blue3: FieldMonitorData = Blue3Default;
+	let red1: FieldMonitorData = Red1Default;
+	let red2: FieldMonitorData = Red2Default;
+	let red3: FieldMonitorData = Red3Default;
+
+	let matchNumber = 999;
+	let matchStatus = MatchStateType.WaitingForPrestart;
 
 	let detailView = () => {};
+
+	const signalrConnection = connectFieldMonitor('http://localhost:5000');
+	signalrConnection.onFieldMonitorDataChanged((data) => {
+		for (const team of data) {
+			if (!team) continue;
+
+			if (team.alliance == AllianceType.Red) {
+				if (team.station == StationType.Station1) {
+					red1 = team;
+				} else if (team.station == StationType.Station2) {
+					red2 = team;
+				} else if (team.station == StationType.Station3) {
+					red3 = team;
+				}
+			} else if (team.alliance == AllianceType.Blue) {
+				if (team.station == StationType.Station1) {
+					blue1 = team;
+				} else if (team.station == StationType.Station2) {
+					blue2 = team;
+				} else if (team.station == StationType.Station3) {
+					blue3 = team;
+				}
+			}
+		}
+	});
+	signalrConnection.onMatchStatusInfoChanged((data) => {
+		matchNumber = data.matchNumber ?? 999;
+		matchStatus = data.matchState ?? MatchStateType.NoCurrentlyActiveEvent;
+	});
+
+	function matchStatusToString(status: MatchStateType): string {
+		switch (status) {
+			case MatchStateType.WaitingForPrestart:
+			case MatchStateType.WaitingForPrestartTO:
+				return 'Ready to Pre-Start';
+			case MatchStateType.Prestarting:
+			case MatchStateType.PrestartingTO:
+				return 'Prestarting';
+			case MatchStateType.WaitingForMatchPreview:
+			case MatchStateType.WaitingForMatchPreviewTO:
+				return 'Waiting for Match Preview';
+			case MatchStateType.WaitingForSetAudience:
+			case MatchStateType.WaitingForSetAudienceTO:
+				return 'Waiting for Set Audience';
+			case MatchStateType.WaitingForMatchReady:
+				return 'Match Not Ready';
+			case MatchStateType.WaitingForMatchStart:
+				return 'Match Ready';
+			case MatchStateType.GameSpecificData:
+				return 'Game Specific Data';
+			case MatchStateType.MatchAuto:
+				return 'Auto';
+			case MatchStateType.MatchTransition:
+				return 'Transition';
+			case MatchStateType.MatchTeleop:
+				return 'Teleop';
+			case MatchStateType.MatchCancelled:
+				return 'Aborted';
+			case MatchStateType.WaitingForCommit:
+				return 'Waiting for Commit';
+			case MatchStateType.WaitingForPostResults:
+				return 'Waiting for Post Results';
+			case MatchStateType.NoCurrentlyActiveEvent:
+			case MatchStateType.NoCurrentlyActiveTournamentLevel:
+			case MatchStateType.TournamentLevelComplete:
+				return 'Tournament Level Complete';
+			default:
+				return 'Unknown';
+		}
+	}
 </script>
 
-<div
-	class="grid grid-cols-fieldmonitor lg:grid-cols-fieldmonitor-large gap-0.5 md:gap-1 mx-auto justify-center"
->
-	<div class="col-span-6 lg:col-span-8 flex text-lg md:text-2xl font-semibold">
-		<div class="bg-neutral-700 px-2">M: 999</div>
-		<div class="flex-1 bg-green-600 px-2 text-center">Ready to Pre-Start</div>
-		<div class="bg-neutral-700 px-2">On Time</div>
+{#await signalrConnection.start()}
+	<div class="text-center">Connecting to FMS...</div>
+{:then}
+	<div
+		class="grid grid-cols-fieldmonitor lg:grid-cols-fieldmonitor-large gap-0.5 md:gap-1 mx-auto justify-center"
+	>
+		<div class="col-span-6 lg:col-span-8 flex text-lg md:text-2xl font-semibold">
+			<div class="bg-neutral-700 px-2">M: {matchNumber}</div>
+			<div class="flex-1 bg-green-600 px-2 text-center">{matchStatusToString(matchStatus)}</div>
+			<div class="bg-neutral-700 px-2">On Time</div>
+		</div>
+		<MonitorRow monitorFrame={blue1} {detailView} />
+		<MonitorRow monitorFrame={blue2} {detailView} />
+		<MonitorRow monitorFrame={blue3} {detailView} />
+		<MonitorRow monitorFrame={red1} {detailView} />
+		<MonitorRow monitorFrame={red2} {detailView} />
+		<MonitorRow monitorFrame={red3} {detailView} />
 	</div>
-	<MonitorRow monitorFrame={teams[0] ?? Blue1Default} {detailView} />
-	<MonitorRow monitorFrame={teams[1] ?? Blue2Default} {detailView} />
-	<MonitorRow monitorFrame={teams[2] ?? Blue3Default} {detailView} />
-	<MonitorRow monitorFrame={teams[3] ?? Red1Default} {detailView} />
-	<MonitorRow monitorFrame={teams[4] ?? Red2Default} {detailView} />
-	<MonitorRow monitorFrame={teams[5] ?? Red3Default} {detailView} />
-</div>
+{:catch error}
+	<div class="text-center text-red-500">Error connecting to FMS: {error.message}</div>
+{/await}

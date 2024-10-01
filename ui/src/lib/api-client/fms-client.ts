87ae4173c9321c17d3282e3c509f3f4302204be1
Bryan Herbst <bryan.c.herbst@gmail.com>
2024-10-01 18:46:18 -0500

    Update FMS API client to reflect latest swagger spec changes

--- ui/src/lib/api-client/fms-client.ts
+++ ui/src/lib/api-client/fms-client.ts
@@ -4,7 +4,6 @@ import { settingsStore } from '$lib/settings-store';
 import { get } from 'svelte/store';
 
 export const fmsClient = createClient<paths>({ baseUrl: get(settingsStore).fmsUrl });
-const apiVersion = '1.0';
 
 // TODO sync with FMS - not sure which API to use for that at the moment since `/FTA` seems to only
 // return 0 for currentSeason
@@ -20,37 +19,24 @@ const authMiddleware: Middleware = {
 };
 fmsClient.use(authMiddleware);
 
-// Our swagger specs have some parameters defined as query parameters, but also include those parameters as
-// part of the path. Those path parameters cannot be replaced correctly, so we will need to strip them out.
-const fixPathsMiddleware: Middleware = {
-	async onRequest({ request }) {
-		// Strip any path parameters that haven't been provided
-		// E.g. /teamIssues/{noteId} becomes /teamIssues
-		let fixedUrl = request.url.replace(/\/%7B\w*%7D/g, '');
-		return new Request(fixedUrl, request);
-	}
-};
-fmsClient.use(fixPathsMiddleware);
-
 export type TeamIssue = components['schemas']['TeamIssueModel'];
 export async function getTeamNotes(
 	fetch: any,
 	options: {
 		noteId?: string;
 		teamNumber?: number;
-		issueType?: string;
-		resolutionStatus?: string;
+		issueType?: components['schemas']['EventNoteIssueTypes'];
+		resolutionStatus?: components['schemas']['EventNoteResolutionTypes'];
 	}
 ) {
 	const { data, error, response } = await fmsClient.GET(
-		'/api/v{version}/FTA/{season}/{eventCode}/teamIssues/{noteId}/{teamNumber}/{issueType}/{resolutionStatus}',
+		'/api/v1.0/FTA/{season}/{eventCode}/teamIssues',
 		{
 			params: {
 				query: options,
 				path: {
 					season: season,
 					eventCode: get(settingsStore).eventCode,
-					version: apiVersion
 				}
 			},
 			fetch
@@ -65,14 +51,6 @@ export async function getTeamNotes(
 }
 
 export async function getCurrentEventCode(): Promise<string | null | undefined> {
-	const { data } = await fmsClient.POST('/api/v{version}/FTAAppApi/CurrentEventStatus', {
-		params: {
-			path: {
-				version: apiVersion
-			}
-		},
-		fetch
-	});
-
+	const { data } = await fmsClient.GET('/api/v1.0/FTAAppApi/CurrentEventStatus', { fetch });
 	return data?.eventCode;
 }

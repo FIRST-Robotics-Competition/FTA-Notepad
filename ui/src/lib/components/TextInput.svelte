cc43359a141f38d553e4f21527005dcc57c0b909
Bryan Herbst <bryan.c.herbst@gmail.com>
2025-06-20 14:06:58 -0500

    Fix unreadable text input color in light mode

--- ui/src/lib/components/TextInput.svelte
+++ ui/src/lib/components/TextInput.svelte
@@ -24,7 +24,7 @@
 		{...restProps}
 		type="text"
 		bind:value={text}
-		class="block flex-1 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 py-1.5 px-2 text-gray-900 placeholder:text-gray-400"
+		class="block flex-1 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 dark:focus:ring-indigo-300 py-1.5 px-2 text-gray-900 dark:text-white placeholder:text-gray-400"
 		{placeholder}
 		{onblur}
 	/>
